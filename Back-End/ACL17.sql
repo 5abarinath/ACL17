2f8bef51140ca5322aa234d53c36ad5f06450ca2
vishvanatarajan <vishvanatarajan@gmail.com>
2017-11-13 18:13:26 +0530

    Database created alongwith basic back-end set up files

---
+++ Back-End/ACL17.sql
@@ -0,0 +1,169 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 13, 2017 at 10:26 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ACL17`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Bidders`
+--
+
+CREATE TABLE `Bidders` (
+  `team_id` tinyint(3) UNSIGNED NOT NULL,
+  `team_name` varchar(50) NOT NULL,
+  `team_owner` varchar(50) NOT NULL,
+  `team_logo` varchar(100) NOT NULL,
+  `points_spent` int(10) UNSIGNED NOT NULL,
+  `premium_left` mediumint(8) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Bidding`
+--
+
+CREATE TABLE `Bidding` (
+  `bid_id` mediumint(8) UNSIGNED NOT NULL,
+  `team_id` tinyint(3) UNSIGNED NOT NULL,
+  `bid_price` mediumint(8) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Groups`
+--
+
+CREATE TABLE `Groups` (
+  `group_id` tinyint(3) UNSIGNED NOT NULL,
+  `player_id` smallint(5) UNSIGNED NOT NULL,
+  `group_name` varchar(8) NOT NULL,
+  `base_bid` mediumint(8) UNSIGNED NOT NULL,
+  `max_bid` mediumint(8) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Players`
+--
+
+CREATE TABLE `Players` (
+  `player_id` smallint(5) UNSIGNED NOT NULL,
+  `player_fname` varchar(50) NOT NULL,
+  `player_lname` varchar(50) NOT NULL,
+  `player_image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Teams`
+--
+
+CREATE TABLE `Teams` (
+  `team_id` tinyint(3) UNSIGNED NOT NULL,
+  `player_id` smallint(5) UNSIGNED NOT NULL,
+  `group_id` tinyint(3) UNSIGNED NOT NULL,
+  `team_name` varchar(50) NOT NULL,
+  `player_price` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Bidders`
+--
+ALTER TABLE `Bidders`
+  ADD PRIMARY KEY (`team_id`),
+  ADD KEY `name_index` (`team_name`);
+
+--
+-- Indexes for table `Bidding`
+--
+ALTER TABLE `Bidding`
+  ADD PRIMARY KEY (`bid_id`),
+  ADD UNIQUE KEY `team_id` (`team_id`);
+
+--
+-- Indexes for table `Groups`
+--
+ALTER TABLE `Groups`
+  ADD PRIMARY KEY (`group_id`,`player_id`),
+  ADD UNIQUE KEY `group_name` (`group_name`),
+  ADD KEY `grpname_index` (`group_name`),
+  ADD KEY `playerid_fk1` (`player_id`);
+
+--
+-- Indexes for table `Players`
+--
+ALTER TABLE `Players`
+  ADD PRIMARY KEY (`player_id`);
+
+--
+-- Indexes for table `Teams`
+--
+ALTER TABLE `Teams`
+  ADD PRIMARY KEY (`team_id`,`player_id`),
+  ADD UNIQUE KEY `teamname_index` (`team_name`),
+  ADD UNIQUE KEY `group_id` (`group_id`),
+  ADD KEY `playerid_fk2` (`player_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Bidding`
+--
+ALTER TABLE `Bidding`
+  MODIFY `bid_id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Bidding`
+--
+ALTER TABLE `Bidding`
+  ADD CONSTRAINT `teamid_fk1` FOREIGN KEY (`team_id`) REFERENCES `Bidders` (`team_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `Groups`
+--
+ALTER TABLE `Groups`
+  ADD CONSTRAINT `playerid_fk1` FOREIGN KEY (`player_id`) REFERENCES `Players` (`player_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `Teams`
+--
+ALTER TABLE `Teams`
+  ADD CONSTRAINT `groupid_fk1` FOREIGN KEY (`group_id`) REFERENCES `Groups` (`group_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `playerid_fk2` FOREIGN KEY (`player_id`) REFERENCES `Players` (`player_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `teamid_fk2` FOREIGN KEY (`team_id`) REFERENCES `Bidders` (`team_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `teamname_fk1` FOREIGN KEY (`team_name`) REFERENCES `Bidders` (`team_name`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
