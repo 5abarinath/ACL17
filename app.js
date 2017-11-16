029fa6098d93ce9172de3a0437b89a34b8788d53
vishvanatarajan <vishvanatarajan@gmail.com>
2017-11-16 14:38:21 +0530

    Remove index.ejs. Use JS validation for redirection.

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
-const session = require('express-session');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const passport = require('passport');
 const flash = require('connect-flash');
 const cors = require('cors');
 
@@ -13,9 +11,6 @@ const port = 3000;
 
 // Check connection with MySQL database
 const connection = require('./config/database');
-
-// Configure passport for login
-require('./config/passport')(passport);
 
 // CORS middleware
 app.use(cors());
@@ -27,13 +22,6 @@ app.set('view engine', 'ejs');
 // Set static folder
 app.use(express.static(path.join(__dirname + '/public')));
 
-// Set up session for persistent logins using passport
-app.use(session({
-	secret: 'ACL2017biddingsecret',
-	resave: true,
-	saveUninitialized: true
-}));
-
 // Cookie-Parser middleware
 app.use(cookieParser());
 
@@ -41,16 +29,12 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-// Initialize passport
-app.use(passport.initialize());
-// Set up session using passport for persistent logins
-app.use(passport.session());
-
 // Use connect-flash for flash messages stored in session
 app.use(flash());
 
-// Routes
-require('./routes')(app, passport);
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/public/index.html');
+});
 
 // Start server
 app.listen(port, () => {
