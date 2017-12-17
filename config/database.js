0e4027bed7dd4ad5f3e8f8ceced8005a15bb7837
deepti1997 <deepti.tanpure@gmail.com>
2017-12-17 21:25:03 +0530

    Add end slot sound

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host : 'localhost',
     user : 'root',
-    password : 'v_926',
+    password : 'bhai',
     database : 'ACL17'
 }); 
 
