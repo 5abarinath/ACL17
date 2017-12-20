b721991f827c987b0bc61fc8792f4a3428c6db21
5abarinath <sabarinath1996@gmail.com>
2017-12-20 17:05:25 +0530

    Minor UI changes for improved visibility

--- config/database.js
+++ config/database.js
@@ -3,9 +3,8 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host : 'localhost',
     user : 'root',
-    password : 'v_926',
-    database : 'ACL17',
-    port : '3306'
+    password : '',
+    database : 'ACL17'
 }); 
 
 connection.connect(function(err) {
